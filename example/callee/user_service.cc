8bd56b707651a9f3fb261cf347d9db308a83e90f
luochenhao <luochenhao@huatusoft.com>
2025-12-28 15:39:58 +0800

    update

--- example/callee/user_service.cc
+++ example/callee/user_service.cc
@@ -1,22 +1,26 @@
-// 提供rpc服务
-// 远程方法名字、方法参数、方法返回值序列化与反序列化
 
 #include <string>
 #include <iostream>
 #include "user.pb.h"
 
-class UserService
-{
+class UserService {
 public:
-    bool Login(const std::string& user_name, const std::string& user_pwd) {
-        std::cout << "do local service: login" << std::endl;
-        std::cout << "user_name" << user_name << "user_pwd" << user_pwd << std::endl;
+    bool Login(const std::string& uid, const std::string& password) {
+        // 远程方法执行的具体逻辑
+        // 1. 获取数据库连接
+        // 2. 查询用户名和密码
+        // 3. 如果用户名和密码正确，返回true
+        // 4. 如果用户名和密码不正确，返回false
+        std::cout << "callee do service rpc function: login" << std::endl;
+        std::cout << "uid: " << uid << std::endl;
+        std::cout << "password: " << password << std::endl;
+        return true;
     }
 };
 
-int main()
-{
-
-
-}
+// int main() {
+//     // 调用远程方法
+//     UserService user_service;
+//     user_service.Login("luochenhao", "123456");
+// }
 
